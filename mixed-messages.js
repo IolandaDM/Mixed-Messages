3570a9a7109974ec9a74b892d924d99be1fee97a
IolandaDM <68613203+IolandaDM@users.noreply.github.com>
2022-11-23 15:38:23 +0000

    Correct random messages function use 3 components to compose a message.

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,7 +1,12 @@
 function motivationalMessages(){
-    let messages = ['A', 'B', 'C', 'D', 'E'];
-    let i = Math.floor(Math.random()*messages.length);
-    let randomMessage = messages[i];
+    let part1 = ['A ', 'This ', 'Going for a '];
+     let part2 = ['run ', '5k ', 'speed run '];
+      let part3 = ['will make your day!', 'will pump you up for the day!', 'will wake you up for sure!'];
+    let i = Math.floor(Math.random()*part1.length);
+        let j = Math.floor(Math.random()*part2.length);
+    let k = Math.floor(Math.random()*part3.length);
+
+    let randomMessage = part1[i] + part2[j] + part3[k];
     return randomMessage;
 }
 console.log(motivationalMessages())
